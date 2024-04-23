634dd502462c8e66455954a50693d58cd43143f5
Chadwick91 <89036537+Chadwick91@users.noreply.github.com>
2024-04-23 13:59:01 -0700

    Create project copy main.js

---
+++ project copy main.js
@@ -0,0 +1,15 @@
+let raceNumber = Math.floor(Math.random() * 1000);
+
+let registeredEarly = 'No'
+var age = 33
+
+if (registeredEarly === 'Yes' && age >= 18) {
+    console.log(`Scheduled for 9:30am, race number is ${raceNumber += 1000}`);
+} else if (registeredEarly === 'No' && age >= 18) {
+    console.log(`Late registration runs at 11:00am. Youre race number is ${raceNumber}`);
+} 
+
+if (age < 18); {
+    console.log(`Your race is at 12:30pm, your number is ${raceNumber}`);
+}
+
